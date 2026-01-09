37987e6d8d3cd95eb3da9163637e995cd468c677
Andreas Gajdosik <andreas@gajdosik.org>
2026-01-09 11:56:28 +0100

    Use "Back" instead of arrow in Navigation

--- front/src/lib/Navigation.svelte
+++ front/src/lib/Navigation.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import { page } from '$app/state';
+    import { t } from 'svelte-i18n';
 </script>
 
 <div class="navigation">
-    <button onclick={() => goto('/')} class="navigation" disabled={page.url.pathname==='/'}>↵</button>
+    <button onclick={() => goto('/')} class="navigation" disabled={page.url.pathname==='/'}>{$t('buttons.back')}</button>
 </div>
 
 <style>
 button {
     all: unset;
     min-width: 25px;
+    text-decoration: underline;
 }
 button:hover{
     all: unset;
     min-width: 25px;
     cursor: pointer;
+    text-decoration: underline;
 }
 button:disabled{
     color: #666666;
